faf7346d856373b6ff7a84b214aa0cf6653ce0eb
sanjayav <svithanagama@gmail.com>
2024-01-29 20:57:42 +1100

    cnsia: Initial repo commit for catalog-service.

---
+++ src/test/java/me/sanjayav/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sanjayav.polarbookshop.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
