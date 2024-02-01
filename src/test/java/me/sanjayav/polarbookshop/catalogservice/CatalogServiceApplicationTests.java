c42db8431442243e65ee053a086c45a31f4bbe21
sanjayav <svithanagama@gmail.com>
2024-02-01 22:18:23 +1100

    cnsia: Add test cases.

--- src/test/java/me/sanjayav/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/me/sanjayav/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package me.sanjayav.polarbookshop.catalogservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import me.sanjayav.polarbookshop.catalogservice.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient webTestClient;
 
+	@Test
+	void whenPostRequestThenBookCreated() {
+		var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody().json(
+						"""
+								 {
+								     "isbn": "1231231231",
+								     "title": "Title",
+								     "author": "Author",
+								     "price": 9.9
+								 }
+								""", true);
+	}
 }
