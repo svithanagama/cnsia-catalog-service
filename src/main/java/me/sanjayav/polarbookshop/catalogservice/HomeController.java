ab86338203a245e6106cda4e116cbaecf8c5ff47
sanjayav <svithanagama@gmail.com>
2024-02-20 21:02:33 +1100

    cnsia: Profile example.

--- src/main/java/me/sanjayav/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/me/sanjayav/polarbookshop/catalogservice/HomeController.java
@@ -1,14 +1,21 @@
 package me.sanjayav.polarbookshop.catalogservice;
 
+import me.sanjayav.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("")
     public String getGreeting() {
-        return "Welcome to the book catalog";
+        return polarProperties.getGreeting();
     }
 
 }
