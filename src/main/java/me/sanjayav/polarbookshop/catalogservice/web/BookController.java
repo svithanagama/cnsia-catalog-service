1f4d95147b9c8ce28238911bb2509753f9e91c94
sanjayav <svithanagama@gmail.com>
2025-11-15 15:41:38 +1100

    cnsia: Add log statements to the book controller. (In the real world, this should be done via a separate advice)

--- src/main/java/me/sanjayav/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/me/sanjayav/polarbookshop/catalogservice/web/BookController.java
@@ -3,6 +3,8 @@ package me.sanjayav.polarbookshop.catalogservice.web;
 import jakarta.validation.Valid;
 import me.sanjayav.polarbookshop.catalogservice.domain.Book;
 import me.sanjayav.polarbookshop.catalogservice.domain.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +14,41 @@ public class BookController {
 
     private final BookService bookService;
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
     @GetMapping
     public Iterable<Book> get() {
+        log.info("Fetching the list of books in the catalog");
         return bookService.viewBookList();
     }
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info("Fetching book by isbn={}.", isbn);
         return bookService.viewBookDetails(isbn);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book post(@Valid @RequestBody Book book) {
+        log.info("Creating new book entry for isbn={}.", book.isbn());
         return bookService.addBookToCatalog(book);
     }
 
     @DeleteMapping("{isbn}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable String isbn) {
+        log.info("Deleting book with isbn={}.", isbn);
         bookService.removeBookFromCatalog(isbn);
     }
 
     @PutMapping("{isbn}")
-    public Book put(@PathVariable String isbn,@Valid @RequestBody Book book) {
+    public Book put(@PathVariable String isbn, @Valid @RequestBody Book book) {
+        log.info("Updating book with isbn={}.", isbn);
         return bookService.editBookDetails(isbn, book);
     }
 }
